e8c831fe8341376ac71da2af6567bf4ff45db4da
Archangel2903 <archangel2903@gmail.com>
2020-11-27 21:59:36 +0200

    main page

--- src/js/index.js
+++ src/js/index.js
@@ -35,10 +35,10 @@ $(function () {
                 autoplay: true,
                 spaceBetween: 25,
                 slidesPerView: 1,
-                navigation: {
+                /*navigation: {
                     nextEl: '.swiper-button-next',
                     prevEl: '.swiper-button-prev'
-                },
+                },*/
                 pagination: {
                     el: '.swiper-pagination',
                     clickable: true
