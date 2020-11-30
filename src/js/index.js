d9281a8eccfa451cf38104d852281b69cafefbf4
Archangel2903 <archangel2903@gmail.com>
2020-12-01 00:26:05 +0200

    main page

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,11 @@ import $ from 'jquery';
 import 'jquery-ui'
 import 'jquery-ui/ui/effect'
 import 'bootstrap';
+import 'bootstrap-star-rating';
 import 'popper.js';
 import Swiper from 'swiper/dist/js/swiper.min';
 import noUiSlider from 'nouislider';
+import IMask from 'imask';
 
 
 $(window).on('load', function () {
@@ -99,6 +101,56 @@ $(function () {
             });
         }
     }
+
+    // Input mask
+    let element = document.querySelectorAll('.mask-phone');
+    element.forEach(function (e) {
+        let mask = IMask(e, {
+            mask: '{+7}(000)000-00-00',
+            lazy: false,
+            placeholderChar: '_'
+        });
+
+        let placeholder = '+7(';
+        e.onfocus = function () {
+            if (this.value === placeholder || this.value === '') {
+                this.value = placeholder
+            }
+        };
+        e.onblur = function () {
+            if (this.value === placeholder) {
+                this.value = ''
+            }
+        };
+    });
+
+    // star rating
+    let rateInput = document.querySelectorAll('.rating-input');
+    rateInput.forEach(function (input) {
+        if (input.classList.contains('rating-read')) {
+            $(input).rating({
+                showCaption: false,
+                showClear: false,
+                displayOnly: true,
+                max: 5,
+                size: 'xs',
+                emptyStar: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" enable-background="new 0 0 512 512"><path fill-rule="evenodd" clip-rule="evenodd" fill="#936f62" d="M256 19l84.4 148.9L512 200 392.5 324.2 414.2 493 256 420.8 97.7 493l21.7-168.8L0 200l171.6-32.1L256 19"/></svg>',
+                filledStar: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" enable-background="new 0 0 512 512"><path fill-rule="evenodd" clip-rule="evenodd" fill="#317b0c" d="M256 19l84.4 148.9L512 200 392.5 324.2 414.2 493 256 420.8 97.7 493l21.7-168.8L0 200l171.6-32.1L256 19"/></svg>',
+            });
+        } else {
+            $(input).rating({
+                showCaption: false,
+                showClear: false,
+                stars: 5,
+                min: 0,
+                max: 5,
+                step: 1,
+                size: 'xs',
+                emptyStar: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" enable-background="new 0 0 512 512"><path fill-rule="evenodd" clip-rule="evenodd" fill="#936f62" d="M256 19l84.4 148.9L512 200 392.5 324.2 414.2 493 256 420.8 97.7 493l21.7-168.8L0 200l171.6-32.1L256 19"/></svg>',
+                filledStar: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" enable-background="new 0 0 512 512"><path fill-rule="evenodd" clip-rule="evenodd" fill="#317b0c" d="M256 19l84.4 148.9L512 200 392.5 324.2 414.2 493 256 420.8 97.7 493l21.7-168.8L0 200l171.6-32.1L256 19"/></svg>',
+            });
+        }
+    });
 
     // Lazy load observer
     const imagesAll = document.querySelectorAll('img[data-src]');
